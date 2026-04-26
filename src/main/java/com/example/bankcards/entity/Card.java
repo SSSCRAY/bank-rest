9622792849c6b3a740881f69e26572aa829065fc
artem <artemcherepanov551@gmail.com>
2026-04-26 12:53:05 +0500

    feat: add JPA entities User, Role and Card, Liquibase migrations, repositories

---
+++ src/main/java/com/example/bankcards/entity/Card.java
@@ -0,0 +1,37 @@
+package com.example.bankcards.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "cards")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "card_number_encrypted")
+    private String cardNumberEncrypted;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private CardStatus status;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal balance;
+
+}
